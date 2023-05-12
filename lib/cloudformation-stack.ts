78abe966b9dfa9212cefebf0019d194f6b3f8e9b
Michael Hall <michael@michaelhall.rocks>
2023-05-11 21:42:45 -0600

    base

--- lib/cloudformation-stack.ts
+++ lib/cloudformation-stack.ts
@@ -1,11 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { LambdaLayerStack } from './lambda';
 
 export class CloudformationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    const app = new cdk.App();
+    new LambdaLayerStack(app, 'ExampleLambdaStack')
     // The code that defines your stack goes here
 
     // example resource
